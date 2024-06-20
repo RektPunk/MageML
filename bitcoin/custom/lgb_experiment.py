bdb568a3db118f8836189da79592feab1b1e8cbb
RektPunk <rektpunk@gmail.com>
2024-06-20 17:51:58 +0900

    feat - update bitcoin

--- bitcoin/custom/lgb_experiment.py
+++ bitcoin/custom/lgb_experiment.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Union, Dict
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_auc_score
 import lightgbm as lgb
